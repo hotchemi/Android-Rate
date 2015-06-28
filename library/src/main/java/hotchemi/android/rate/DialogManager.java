38da532f3935949afe4f5e14ca72038b0056f774
Shintaro Katafuchi <hot.chemistry21@gmail.com>
2015-06-28 19:03:27 +0900

    Fix the bug that button text overflow when the device is over lollipop.

--- library/src/main/java/hotchemi/android/rate/DialogManager.java
+++ library/src/main/java/hotchemi/android/rate/DialogManager.java
@@ -1,25 +1,29 @@
 package hotchemi.android.rate;
 
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.view.View;
-import android.view.Window;
 
 import static hotchemi.android.rate.IntentHelper.createIntentForGooglePlay;
 import static hotchemi.android.rate.PreferenceHelper.setAgreeShowDialog;
 import static hotchemi.android.rate.PreferenceHelper.setRemindInterval;
+import static hotchemi.android.rate.Utils.getDialogBuilder;
 
 final class DialogManager {
 
     private DialogManager() {
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     static Dialog create(final Context context, boolean isShowNeutralButton,
                          boolean isShowTitle, final OnClickButtonListener listener, View view) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        AlertDialog.Builder builder = getDialogBuilder(context);
         builder.setMessage(R.string.rate_dialog_message);
+        if (isShowTitle) builder.setTitle(R.string.rate_dialog_title);
         if (view != null) builder.setView(view);
         builder.setPositiveButton(R.string.rate_dialog_ok, new DialogInterface.OnClickListener() {
             @Override
@@ -45,13 +49,7 @@ final class DialogManager {
                 if (listener != null) listener.onClickButton(which);
             }
         });
-        AlertDialog dialog = builder.create();
-        if (!isShowTitle) {
-            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        } else {
-            dialog.setTitle(R.string.rate_dialog_title);
-        }
-        return dialog;
+        return builder.create();
     }
 
 }
