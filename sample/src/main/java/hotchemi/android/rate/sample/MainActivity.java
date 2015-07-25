3271d92bb9bc023849b34d8e9d973b527526ffb6
Shintaro Katafuchi <hot.chemistry21@gmail.com>
2015-07-26 08:48:40 +0900

    Delete appcompat depencency from sample.

--- sample/src/main/java/hotchemi/android/rate/sample/MainActivity.java
+++ sample/src/main/java/hotchemi/android/rate/sample/MainActivity.java
@@ -1,13 +1,13 @@
 package hotchemi.android.rate.sample;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import hotchemi.android.rate.AppRate;
 import hotchemi.android.rate.OnClickButtonListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
